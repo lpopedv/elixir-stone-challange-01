f5045482746c14f4822a88e55879e36f6ea27c4d
Lucas Pope <lpopedv@proton.me>
2024-10-10 16:43:05 -0300

    feat: module definition

---
+++ elixir-na-pratica-01.exs
@@ -0,0 +1,19 @@
+defmodule Challange do
+  @moduledoc """
+  Módulo do desafio
+  """
+
+  @spec split(
+    shopping_list = [
+      {
+        item :: String.t(),
+        init_price_in_cents :: pos_integer()
+      }
+    ],
+
+    emails :: [String.t()]
+  ) :: [%{String.t() => non_neg_integer()}]
+  def split() do
+    raise "not implemented"
+  end
+end
